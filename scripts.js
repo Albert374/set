6a954d5f881b72ae8c2b4d10c3a79e032422fa41
Albert374 <likenice122@gmail.com>
2024-10-12 02:01:43 -0700

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,25 +1,15 @@
-document.getElementById('registroForm').addEventListener('submit', function(event) {
+(function(){
+    emailjs.init("clcXbpntO9q4ne1tt"); 
+})();
+
+document.getElementById('miFormulario').addEventListener('submit', function(event) {
     event.preventDefault(); 
-
-    const nombre = document.getElementById('nombre').value;
-    const telefono = document.getElementById('telefono').value;
-
-    emailjs.send('service_n0toocd', 'template_avf7ioc', {
-        nombre: nombre,
-        telefono: telefono
-    }).then(function(response) {
-        console.log('Éxito!', response.status);
-        document.getElementById('mensaje-enviado').style.display = 'block'; 
-        document.getElementById('registroForm').reset(); 
-
-        // Cambiar de sección
-        document.getElementById('registro').style.display = 'none'; 
-        document.getElementById('seccionPrincipal').style.display = 'block'; 
-    }, function(error) {
-        console.log('Error:', error);
-        document.getElementById('mensaje-error').style.display = 'block'; 
-    });
+    
+   
+    emailjs.sendForm('service_n0toocd', 'template_avf7ioc', this)
+        .then(function() {
+            alert('Correo enviado exitosamente!');
+        }, function(error) {
+            alert('Error al enviar el correo: ' + JSON.stringify(error));
+        });
 });
-
-// Inicializar EmailJS
-emailjs.init('clcXbpntO9q4ne1tt');
