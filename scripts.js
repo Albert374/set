510fc482069bf1140e51e0ac2296b9835433ea34
Albert374 <likenice122@gmail.com>
2024-10-12 01:51:00 -0700

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,25 +1,25 @@
 document.getElementById('registroForm').addEventListener('submit', function(event) {
-    event.preventDefault(); // Evita el envío tradicional del formulario
+    event.preventDefault(); 
 
     const nombre = document.getElementById('nombre').value;
     const telefono = document.getElementById('telefono').value;
 
-    emailjs.send("service_n0toocd", "template_avf7ioc", {
+    emailjs.send('service_n0toocd', 'template_avf7ioc', {
         nombre: nombre,
         telefono: telefono
     }).then(function(response) {
         console.log('Éxito!', response.status);
-        document.getElementById('mensaje-enviado').style.display = 'block'; // Mensaje de éxito
-        document.getElementById('registroForm').reset(); // Limpiar formulario
+        document.getElementById('mensaje-enviado').style.display = 'block'; 
+        document.getElementById('registroForm').reset(); 
 
         // Cambiar de sección
-        document.getElementById('registro').style.display = 'none'; // Oculta el registro
-        document.getElementById('seccionPrincipal').style.display = 'block'; // Muestra la sección principal
+        document.getElementById('registro').style.display = 'none'; 
+        document.getElementById('seccionPrincipal').style.display = 'block'; 
     }, function(error) {
         console.log('Error:', error);
-        document.getElementById('mensaje-error').style.display = 'block'; // Mensaje de error
+        document.getElementById('mensaje-error').style.display = 'block'; 
     });
 });
 
 // Inicializar EmailJS
-emailjs.init("clcXbpntO9q4ne1tt"); // Reemplaza con tu Public Key
+emailjs.init('clcXbpntO9q4ne1tt');
